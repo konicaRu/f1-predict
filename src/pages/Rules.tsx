0bf3e35f2e8da103cf4d0f576c758ef8903b70b5
Dim <prokol35@gmail.com>
2026-07-22 15:35:00 +0300

    fix: переиспользовать .page-h1 вместо дублирующего .rules-title

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -27,7 +27,7 @@ const STRATEGY: string[] = [
 export default function Rules() {
   return (
     <div className="rules">
-      <h1 className="rules-title">🏁 Как считаются очки</h1>
+      <h1 className="page-h1">🏁 Как считаются очки</h1>
 
       <div className="rules-table-wrap">
         <table className="rules-table">
